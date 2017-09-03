ed9c437c4a52a7435a56f5bd47dcc93a5a590551
Laurent Le Graverend <laurent@legraverend.fr>
2017-09-03 21:44:24 +0800

    Cleanup old sources, set new basic project

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
-const gulp = require('gulp');
-const gutil = require('gulp-util');
-const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const minifycss = require('gulp-minify-css');
+const browserSync = require('browser-sync').create();
+const cleanCss = require('gulp-clean-css');
+const del = require('del');
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const inlinesource = require('gulp-inline-source');
+const usemin = require('gulp-usemin');
+const gutil = require('gulp-util');
 const plumber = require('gulp-plumber');
+const runSequence = require('run-sequence');
+const sass = require('gulp-sass');
 
-// Compile Sass, Autoprefix and minify
+gulp.task('clean', function() {
+  return del(['dist']);
+});
+
 gulp.task('styles', () => {
-  return gulp.src('./styles/**/*.scss')
+  return gulp.src('./src/**/*.scss')
     .pipe(plumber((error) => {
       gutil.log(gutil.colors.red(error.message));
       this.emit('end');
@@ -17,19 +27,70 @@ gulp.task('styles', () => {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./css/'));
+    .pipe(cleanCss())
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.stream());
 });
 
-// Default task
-gulp.task('default', () => {
-  gulp.start('styles');
+gulp.task('scripts', () => {
+  return gulp.src('./src/**/*.js')
+    .pipe(gulp.dest('./dist/'));
 });
 
-// Watch files for changes
-gulp.task('watch', () => {
-
-  // Watch .scss files
-  gulp.watch('./styles/**/*.scss', ['styles']);
-
+gulp.task('html', ['scripts'], () => {
+  return gulp.src('src/**/*.html')
+    .pipe(usemin({
+      html: [() => {
+        return htmlmin({
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: true,
+          removeCommentsFromCDATA: true
+        });
+      }]
+    }))
+    .pipe(inlinesource())
+    .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('copyOthers', () => {
+  return gulp.src([
+    'src/humans.txt'
+  ])
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('src/**/*.{png,jpg,svg,gif}')
+    .pipe(imagemin([
+      imagemin.svgo({ plugins: [{ removeTitle: true, removeViewBox: true }] })
+    ]))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('serve', () => {
+  return runSequence(
+    'clean',
+    ['html', 'images', 'styles', 'scripts'], () => {
+      browserSync.init({
+        ghostMode: false,
+        server: './dist'
+      });
+
+      gulp.watch('./src/**/*.scss', ['styles']);
+      gulp.watch('./src/**/*.js', ['scripts', 'html']);
+      gulp.watch('./src/**/*.js').on('change', browserSync.reload);
+      gulp.watch('./src/*.html').on('change', browserSync.reload);
+    });
+});
+
+gulp.task('build', () => {
+  return runSequence(
+    'clean',
+    ['html', 'images', 'styles', 'scripts', 'copyOthers']
+  );
+});
+
+gulp.task('default', ['serve']);
